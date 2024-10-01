289d1879c5108c930c931e1d4389e372eb173fd0
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-10-01 17:10:24 +0200

    environmemt

---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig :{
+  apiKey: "AIzaSyCfaL4sAKyQtzo82DC_IepPbInFV8ZRDAw",
+  authDomain: "fir-blog-crud-efb04.firebaseapp.com",
+  projectId: "fir-blog-crud-efb04",
+  storageBucket: "fir-blog-crud-efb04.appspot.com",
+  messagingSenderId: "1028689862546",
+  appId: "1:1028689862546:web:db62022e28b0f5d76f6b4b",
+  measurementId: "G-9BSVK2CX5J"
+}
+};
