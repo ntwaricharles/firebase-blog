695a94240be811302d3931ce2f0210ec7e16bbec
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-10-01 22:26:49 +0200

    blog posts

--- src/app/blog/blog-post/blog-post.component.ts
+++ src/app/blog/blog-post/blog-post.component.ts
@@ -1,45 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BlogService, BlogPost } from '../../services/blog.service';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-post',
   templateUrl: './blog-post.component.html',
 })
-export class BlogPostComponent {
+export class BlogPostComponent implements OnInit {
   title = '';
   content = '';
-  posts$: Observable<BlogPost[]>;
+  posts: BlogPost[] = [];
+  post: BlogPost | undefined;
+  comments: any[] = [];
   editingPostId: string | undefined;
+  loading = false;
+  successMessage: string | undefined;
+  errorMessage: string | undefined;
+  validationError: string | undefined;
 
-  constructor(private blogService: BlogService) {
-    this.posts$ = this.blogService.getPosts();
+  constructor(
+    private blogService: BlogService,
+    private route: ActivatedRoute,
+    public router: Router // Change to public
+  ) {}
+
+  ngOnInit() {
+    this.blogService.getPosts().subscribe((posts) => {
+      this.posts = posts;
+    });
+
+    const postId = this.route.snapshot.paramMap.get('id');
+    if (postId) {
+      this.loadPostDetails(postId);
+    }
   }
 
-  createPost() {
-    const newPost: BlogPost = {
-      title: this.title,
-      content: this.content,
-      author: 'Anonymous',
-      createdAt: new Date(),
-    };
-    this.blogService.createPost(newPost).then(() => {
-      this.resetForm();
-      alert('Post created!');
+  logPostId(postId: string | undefined) {
+    if (postId) {
+      console.log('Navigating to post ID:', postId);
+      this.router.navigate(['/blog', postId]);
+    } else {
+      console.error('Post ID is undefined or null!');
+    }
+  }
+
+  loadPostDetails(postId: string) {
+    this.blogService.getPostById(postId).subscribe((post: BlogPost | null) => {
+      if (post) {
+        this.post = post;
+      }
     });
   }
 
-  editPost(post: BlogPost) {
-    this.title = post.title;
-    this.content = post.content;
-    this.editingPostId = post.id;
+  handleSubmitPost(newPost: BlogPost) {
+    this.loading = true;
+    if (this.editingPostId) {
+      this.blogService
+        .updatePost(this.editingPostId, newPost)
+        .then(() => {
+          this.resetForm();
+          this.successMessage = 'Post updated successfully!';
+          this.loading = false;
+        })
+        .catch((error) => {
+          this.errorMessage = 'Failed to update post.';
+          this.loading = false;
+        });
+    } else {
+      this.blogService
+        .createPost(newPost)
+        .then(() => {
+          this.resetForm();
+          this.successMessage = 'Post created successfully!';
+          this.loading = false;
+        })
+        .catch((error) => {
+          this.errorMessage = 'Failed to create post.';
+          this.loading = false;
+        });
+    }
   }
 
-  deletePost(postId: string | undefined) {
+  deletePost(postId: string | null | undefined) {
     if (postId) {
-      this.blogService.deletePost(postId).then(() => alert('Post deleted!'));
+      this.blogService
+        .deletePost(postId)
+        .then(() => alert('Post deleted!'))
+        .catch((error) => alert('Failed to delete post: ' + error.message));
     } else {
-      alert('Error: Post ID is undefined.');
+      alert('Error: Post ID is undefined or null.');
+    }
+  }
+
+  editPost(post: BlogPost) {
+    if (post.id) {
+      this.router.navigate(['/edit-post', post.id]);
+    } else {
+      alert('Error: Post ID is missing.');
     }
   }
 
@@ -47,6 +104,8 @@ export class BlogPostComponent {
     this.title = '';
     this.content = '';
     this.editingPostId = undefined;
+    this.successMessage = undefined;
+    this.errorMessage = undefined;
+    this.validationError = undefined;
   }
 }
-
