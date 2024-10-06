5ed2587d7a01f4591bbcb2608ba783e728b90dcb
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-10-06 21:24:40 +0200

    improved blog creation with author

--- src/app/blog/post-form/post-form.component.ts
+++ src/app/blog/post-form/post-form.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BlogService } from '../../services/blog.service';
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-post-form',
@@ -14,29 +15,38 @@ export class PostFormComponent implements OnInit {
   loading: boolean = false;
   errorMessage: string | null = null;
   successMessage: string | null = null;
+  authorEmail: string = 'Anonymous';
 
   constructor(
     private blogService: BlogService,
     private router: Router,
     private route: ActivatedRoute,
     private fb: FormBuilder,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private authService: AuthService
   ) {
     this.postForm = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(3)]],
-      content: ['', [Validators.required, Validators.minLength(10)]], 
+      content: ['', [Validators.required, Validators.minLength(10)]],
     });
   }
 
   ngOnInit() {
+    // Get the post ID from the route if we are editing a post
     this.route.params.subscribe((params) => {
       this.postId = params['id'];
       if (this.postId) {
         this.loadPost(this.postId);
       }
     });
+
+    // Get the logged-in user's email and set it as the author, otherwise 'Anonymous'
+    this.authService.getUser().subscribe((user) => {
+      this.authorEmail = user?.email || 'Anonymous';
+    });
   }
 
+  // Load post for editing
   loadPost(id: string) {
     this.blogService.getPostById(id).subscribe((post) => {
       if (post) {
@@ -48,11 +58,9 @@ export class PostFormComponent implements OnInit {
     });
   }
 
+  // Handle form submission for both creating and editing a post
   handleSubmit() {
     this.loading = true;
-
-    console.log('Title:', this.postForm.value.title);
-    console.log('Content:', this.postForm.value.content);
 
     if (this.postForm.invalid) {
       this.loading = false;
@@ -60,9 +68,16 @@ export class PostFormComponent implements OnInit {
       return;
     }
 
+    const postData = {
+      ...this.postForm.value,
+      author: this.authorEmail,
+      createdAt: new Date(),
+    };
+
     if (this.postId) {
+      // Update post
       this.blogService
-        .updatePost(this.postId, this.postForm.value)
+        .updatePost(this.postId, postData)
         .then(() => {
           this.loading = false;
           this.toastr.success('Post updated successfully!');
@@ -73,12 +88,9 @@ export class PostFormComponent implements OnInit {
           this.toastr.error('Failed to update post.');
         });
     } else {
+      // Create new post
       this.blogService
-        .createPost({
-          ...this.postForm.value,
-          author: 'Anonymous',
-          createdAt: new Date(),
-        })
+        .createPost(postData)
         .then(() => {
           this.loading = false;
           this.toastr.success('Post created successfully!');
