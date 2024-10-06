0391944f0a7c35516ee1bf9e1787ff7d0969b2fa
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-10-06 14:37:42 +0200

    user profile

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+})
+export class ProfileComponent implements OnInit {
+  user$: Observable<any> | null = null; // Initialize with null
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user$ = this.authService.getUser();
+  }
+}
